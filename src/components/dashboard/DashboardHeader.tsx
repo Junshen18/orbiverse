20ebdebcb74c173ef91684115bdf298847ff8d67
Junshen18 <junshenhere@gmail.com>
2025-01-05 02:05:37 +0800

    modified for mobile

--- src/components/dashboard/DashboardHeader.tsx
+++ src/components/dashboard/DashboardHeader.tsx
@@ -7,18 +7,24 @@ const router = useRouter();
   const { publicKey } = useWallet();
   return (
     <div className="flex justify-between items-center mb-8">
-      <div>
-        <h1 className="text-xl font-bold mb-2">Orbiverse</h1>
+      
+      <div onClick={()=>{
+        router.push("/authenticated-pages/dashboard");
+      }} className="cursor-pointer flex flex-col items-start">
+        <p className="md:text-4xl font-bold text-3xl">Orb Vault</p>
+        <p className="hidden md:block text-sm text-gray-400">Manage and explore your memory orbs</p>
       </div>
-      <div className="flex flex-col items-center">
-        <p className="text-4xl font-bold items-center">Orb Vault</p>
-        <p className="text-sm text-gray-400">Manage and explore your memory orbs</p>
 
+      <div>
+        <h1 className="text-4xl font-bold mb-2 hidden md:block">Orbiverse</h1>
       </div>
       <div className="flex items-center gap-2">
-        <button className="px-4 py-2 bg-white/10 hover:bg-white/20 border border-white/20 rounded-full" onClick={() => router.push('/authenticated-pages/createorb')}>
+        <div className="hidden md:block cursor-pointer px-4 py-2 bg-white/10 hover:bg-white/20 border border-white/20 rounded-full" onClick={() => router.push('/authenticated-pages/createorb')}>
           Create New Orb
-        </button>
+        </div>
+        <div className="block md:hidden cursor-pointer px-4 py-2 bg-white/10 hover:bg-white/20 border border-white/20 rounded-full" onClick={() => router.push('/authenticated-pages/createorb')}>
+          +
+        </div>
         <div className="font-chillax font-medium px-4 py-0 bg-white/10 hover:bg-white/20 border border-white/20 rounded-full text-sm">
           <WalletMultiButton
             style={{
