17eb5960efef235663f5cd0a77a2d3c0605bd901
Junshen18 <junshenhere@gmail.com>
2025-01-04 15:42:52 +0800

    hot fix features section mobile view

--- src/components/sections/FeaturesSection.tsx
+++ src/components/sections/FeaturesSection.tsx
@@ -30,14 +30,14 @@ export function FeaturesSection() {
         <h2 className="text-3xl md:text-4xl font-bold text-center md:mb-12">
           Why Choose Orbiverse?
         </h2>
-        <div className="relative flex flex-col justify-center">
+        <div className="relative flex flex-col justify-center items-center">
           <div className="w-full h-[300px] md:h-[500px] block md:absolute z-10 pointer-events-none">
             <SplineViewer
               url="https://prod.spline.design/bsKVBJe8ZeafTv8B/scene.splinecode"
               className="w-full h-full pointer-events-auto"
             />
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-2 gap-x-5 gap-y-5 md:gap-x-96 md:gap-y-10 relative z-0">
+          <div className="grid grid-cols-2 gap-x-5 gap-y-5 md:gap-x-72 md:gap-y-10 relative z-0">
             {features.map((feature, index) => (
               <div
                 key={index}
