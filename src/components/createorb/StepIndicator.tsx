106864d2c5c3f6b8ec1e0441899c1bc52cd3576e
Junshen18 <junshenhere@gmail.com>
2025-01-05 14:11:27 +0800

    add img and modified step indicator

--- src/components/createorb/StepIndicator.tsx
+++ src/components/createorb/StepIndicator.tsx
@@ -10,11 +10,11 @@ type StepIndicatorProps = {
 
 export function StepIndicator({ steps, currentStep }: StepIndicatorProps) {
   return (
-    <div className="flex justify-center items-center gap-4">
+    <div className="flex justify-center items-center gap-2 md:gap-4">
       {steps.map((step, index) => (
-        <div key={step.number} className="flex items-center">
+        <div key={step.number} className="flex items-center w-auto">
           <div
-            className={`flex items-center justify-center w-10 h-10 rounded-full ${
+            className={`flex items-center justify-center w-7 h-7 md:w-10 md:h-10 rounded-full text-xs md:text-base ${
               step.number <= currentStep
                 ? 'bg-white/20 text-white'
                 : 'bg-white/5 text-white/50'
@@ -22,10 +22,10 @@ export function StepIndicator({ steps, currentStep }: StepIndicatorProps) {
           >
             {step.number}
           </div>
-          <span className="ml-2 text-sm">{step.title}</span>
+          <span className="ml-2 text-xs md:text-sm ">{step.title}</span>
           {index < steps.length - 1 && (
             <div
-              className={`w-20 h-[1px] mx-4 ${
+              className={`w-6 md:w-20 h-[1px] md:mx-4 ${
                 step.number < currentStep ? 'bg-white/20' : 'bg-white/5'
               }`}
             />
